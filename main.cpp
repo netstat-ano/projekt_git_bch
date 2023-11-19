078ec0d0060676cf5cdf10375cf7a5045d2fd806
netstat-ano <bartosz147258@gmail.com>
2023-11-19 13:01:59 +0100

    Zamiana dodawnia na mnozenie w metodzie policz

--- main.cpp
+++ main.cpp
@@ -2,11 +2,11 @@
 
 
 int policz(int a, int b){
-    return a+b;
+    return a*b; //zamiana dodawania na mnozenie
 }
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    std::cout << "W pierwszym branchu, 2 + 3 = " << policz(2,3) <<std::endl;
+    std::cout << "W pierwszym branchu, 2 * 3 = " << policz(2,3) <<std::endl;
     return 0;
 }
