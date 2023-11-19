89f13d960a5164378ecc6ada58ead8728fcbb8ba
netstat-ano <bartosz147258@gmail.com>
2023-11-19 12:51:48 +0100

    usuniecie gitignore

---
+++ cmake-build-debug/CMakeFiles/projekt_git_bch.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projekt_git_bch.dir/main.cpp.o"
+  "projekt_git_bch"
+  "projekt_git_bch.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/projekt_git_bch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/projekt_git_bch.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for projekt_git_bch.
+# This may be replaced when dependencies are built.
